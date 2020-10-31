12239041016f1888d6e5c434095e6c4898dc18ad
KonTroAl <troshenkin.k@yandex.ru>
2020-10-31 14:00:33 +0500

    completed task 3

--- task_3.py
+++ task_3.py
@@ -14,3 +14,19 @@
 р
 а
 """
+import hashlib
+
+
+def lines(a):
+    my_list = list(a)
+    c = set()
+    for i in range(len(my_list)):
+        for j in range(len(my_list)-1 if i == 0 else len(my_list), i, -1):
+            c.add(hashlib.sha3_256((''.join(my_list[i:j]).encode())).hexdigest())
+            # c.add((''.join(my_list[i:j])))
+    return len(c)
+
+
+my_str = 'papa'
+print(lines(my_str))
+
